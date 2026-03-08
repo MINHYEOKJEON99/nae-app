5e816b4d775b68e5f187bcff5aa8530d45e4274d
MINHYEOKJEON99 <mari394337@gmail.com>
2026-03-08 23:20:14 +0900

    fix: bottomBar width 수정

--- components/layout/BottomTabBar.tsx
+++ components/layout/BottomTabBar.tsx
@@ -1,9 +1,9 @@
-import TabItem from './TabItem';
-import { BriefingIcon, TrendIcon, TodoIcon } from '@/components/common/IconSet';
+import TabItem from "./TabItem";
+import { BriefingIcon, TrendIcon, TodoIcon } from "@/components/common/IconSet";
 
 export default function BottomTabBar() {
   return (
-    <nav className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full max-w-[480px] h-14 safe-area-bottom bg-fg border-t border-border flex items-center z-100">
+    <nav className="fixed bottom-0 left-1/2 -translate-x-1/2 w-full h-14 safe-area-bottom bg-fg border-t border-border flex items-center z-100">
       <TabItem href="/today" label="Today AI" icon={<BriefingIcon size={22} />} />
       <TabItem href="/trends" label="Trends" icon={<TrendIcon size={22} />} />
       <TabItem href="/todo" label="Todo" icon={<TodoIcon size={22} />} />
