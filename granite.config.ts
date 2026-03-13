496975cb5d9182063b1acd0b683d2d6cac4e9e1a
MINHYEOKJEON99 <mari394337@gmail.com>
2026-03-14 00:09:54 +0900

    icon 변경

--- granite.config.ts
+++ granite.config.ts
@@ -5,10 +5,10 @@ export default defineConfig({
   brand: {
     displayName: "하루 5분: IT 트렌드", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#3182F6", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: "/newspaper_icon_logo_600_1.png",
+    icon: "https://static.toss.im/appsintoss/27519/51596f09-675c-4483-a453-a8386c604fc2.png",
   },
   web: {
-    host: "192.168.45.186",
+    host: "192.168.123.108",
     port: 5173,
     commands: {
       dev: "next dev --port 5173",
