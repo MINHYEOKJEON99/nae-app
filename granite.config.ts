76c09b0439939f6584f8b065fe9e865a64d70260
MINHYEOKJEON99 <mari394337@gmail.com>
2026-03-25 23:11:28 +0900

    fix: 앱 이름 수정

--- granite.config.ts
+++ granite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@apps-in-toss/web-framework/config";
 export default defineConfig({
   appName: "my-news",
   brand: {
-    displayName: "하루 5분: IT 트렌드", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
+    displayName: "하루 5분: IT트렌드", // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: "#3182F6", // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
     icon: "https://static.toss.im/appsintoss/27519/51596f09-675c-4483-a453-a8386c604fc2.png",
   },
